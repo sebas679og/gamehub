d014003096bdeac10c39950af52a65297ade70e3
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2025-06-16 17:00:03 -0500

    feat: configure authentication via username

--- src/main/java/com/group4/gamehub/config/CustomUserDetailsService.java
+++ src/main/java/com/group4/gamehub/config/CustomUserDetailsService.java
@@ -16,11 +16,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getEmail())
+                .username(user.getUsername())
                 .password(user.getPassword())
                 .roles(user.getRole().name())
                 .build();
