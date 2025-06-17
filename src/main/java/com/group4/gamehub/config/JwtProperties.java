0493d7166ad7de79fbab8b65a66b1e4ef00f0517
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2025-06-16 19:59:59 -0500

    update jwtProperties

--- src/main/java/com/group4/gamehub/config/JwtProperties.java
+++ src/main/java/com/group4/gamehub/config/JwtProperties.java
@@ -3,26 +3,15 @@ package com.group4.gamehub.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Configuration
 @ConfigurationProperties(prefix = "jwt")
+@Getter
+@Setter
 public class JwtProperties {
 
     private String secret;
     private long expiration;
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
-    public long getExpiration() {
-        return expiration;
-    }
-
-    public void setExpiration(long expiration) {
-        this.expiration = expiration;
-    }
 }
