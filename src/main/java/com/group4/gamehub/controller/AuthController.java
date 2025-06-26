89011ba369e0825f01aa6a87909cc5271d63aab8
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2025-06-25 22:34:38 -0500

    delete duplicate code

--- src/main/java/com/group4/gamehub/controller/AuthController.java
+++ src/main/java/com/group4/gamehub/controller/AuthController.java
@@ -1,22 +1,24 @@
 package com.group4.gamehub.controller;
 
-import com.group4.gamehub.dto.requests.LoginRequest;
-import com.group4.gamehub.dto.requests.RegisterRequest;
-import com.group4.gamehub.dto.responses.AuthResponse;
-import com.group4.gamehub.dto.responses.ErrorResponse;
-import com.group4.gamehub.service.AuthService.AuthServiceInterface;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.group4.gamehub.dto.requests.LoginRequest;
+import com.group4.gamehub.dto.requests.RegisterRequest;
+import com.group4.gamehub.dto.responses.AuthResponse;
+import com.group4.gamehub.dto.responses.ErrorResponse;
+import com.group4.gamehub.service.AuthService.AuthServiceInterface;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -41,14 +43,14 @@ public class AuthController {
             description = "User registered",
             content =
                 @Content(
-                    mediaType = "application/json",
+                    mediaType = APPLICATION_JSON,
                     schema = @Schema(implementation = AuthResponse.class))),
         @ApiResponse(
             responseCode = "409",
             description = "Conflict, registered user",
             content =
                 @Content(
-                    mediaType = "application/json",
+                    mediaType = APPLICATION_JSON,
                     schema = @Schema(implementation = ErrorResponse.class)))
       })
   public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
@@ -65,14 +67,14 @@ public class AuthController {
             description = "Successful login",
             content =
                 @Content(
-                    mediaType = "application/json",
+                    mediaType = APPLICATION_JSON,
                     schema = @Schema(implementation = AuthResponse.class))),
         @ApiResponse(
             responseCode = "400",
             description = "Bad request, Incorrect credentials",
             content =
                 @Content(
-                    mediaType = "application/json",
+                    mediaType = APPLICATION_JSON,
                     schema = @Schema(implementation = ErrorResponse.class)))
       })
   public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
