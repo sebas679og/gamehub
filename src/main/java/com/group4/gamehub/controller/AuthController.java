a1f3820d315bf561445fe470c7e1ae7c4cffb78b
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2025-06-25 22:39:39 -0500

    fix: apply style

--- src/main/java/com/group4/gamehub/controller/AuthController.java
+++ src/main/java/com/group4/gamehub/controller/AuthController.java
@@ -1,24 +1,22 @@
 package com.group4.gamehub.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.group4.gamehub.dto.requests.LoginRequest;
 import com.group4.gamehub.dto.requests.RegisterRequest;
 import com.group4.gamehub.dto.responses.AuthResponse;
 import com.group4.gamehub.dto.responses.ErrorResponse;
 import com.group4.gamehub.service.AuthService.AuthServiceInterface;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
